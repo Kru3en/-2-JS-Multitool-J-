ef5d52ea2bc3a614f35f69c2d8a9beb6060639a0
Игорь Городничук <gorodni4uk.igor@mail.ru>
2024-11-19 01:16:54 +0300

    Initial commit

---
+++ -2-JS-Multitool-J-/webpack.config.js
@@ -0,0 +1,50 @@
+const path = require('path')
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
+
+module.exports = {
+	mode: 'development',
+	entry: './js/script.js',
+	output: {
+		path: path.resolve(__dirname, 'dist'),
+		filename: 'bundle.js',
+		assetModuleFilename: 'assets/img/[name][hash][ext][query]',
+	},
+	module: {
+		rules: [
+			{
+				test: /\.scss$/,
+				use: [
+					MiniCssExtractPlugin.loader,
+					{
+						loader: 'css-loader',
+						options: {
+							url: true,
+							sourceMap: true,
+						},
+					},
+					'sass-loader',
+				],
+			},
+			{
+				test: /\.(png|jpe?g|gif|svg)$/i,
+				type: 'asset/resource',
+				generator: {
+					filename: 'assets/img/[name][hash][ext]',
+				},
+			},
+		],
+	},
+	plugins: [
+		new MiniCssExtractPlugin({
+			filename: 'style.css',
+		}),
+	],
+	devServer: {
+		static: {
+			directory: path.resolve(__dirname, 'dist'),
+		},
+		compress: true,
+		port: 9000,
+	},
+	devtool: 'source-map',
+}
